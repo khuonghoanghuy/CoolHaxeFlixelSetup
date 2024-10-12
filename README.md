f98b584c7e703345ab2965548b5951a8952ca29d
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-10-13 00:41:23 +0700

    psych engine 0.6.3 added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CoolHaxeFlixelSetup
 A Cool Haxe Setup for repo
+
+# Needed
+- Install **winge** if you don't have
+- Install **Haxe** if you don't have
+> You should need to know what Haxe you choice then can be using for install from all install bat
+- Windows OS
+
+# When about to install
+If you want to install, be sure that run the bat with administrator!
---
+++ setup_file/fnf/psych_engine_0.6.3_haxe_4.2.5.bat
@@ -0,0 +1,24 @@
+@echo off
+title Psych Engine 0.6.3 Setup - Install Haxelib
+haxelib install lime 8.0.1
+haxelib install openfl 9.2.1
+haxelib install flixel 5.2.2
+haxelib install flixel-addons 3.0.2
+haxelib install flixel-ui 2.5.0
+haxelib install flixel-tools 1.5.1
+haxelib install tjson
+haxelib install hxjsonast
+haxelib install hxcpp
+haxelib install hxcpp-debug-server
+haxelib install hscript
+cls
+title Psych Engine 0.6.3 Setup - Install Git
+winget install --id Git.Git -e --source winget
+cls
+title Psych Engine 0.6.3 Setup - Install Haxelib thought Git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec.git
+cls
+title Psych Engine 0.6.3 Setup - Finished
+pause
